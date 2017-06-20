c4db117cfc9ed6e42404b2fc9b995e4dde3f2192
williamhampton <willhamptonv@gmail.com>
2017-06-20 19:17:14 -0400

    fixed my clock

---
+++ Clock.C
@@ -0,0 +1,20 @@
+/* create a program that shows the time */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+
+int main(){
+  char number[1];
+  time_t timer;
+  char buffer[26];
+  struct tm* tm_info;
+  time(&timer);
+  tm_info = localtime(&timer);
+  strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+  puts(buffer);
+  return 0;
+}
