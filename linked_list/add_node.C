3c96cc9cced87e3335a7273cdc09a74d8731133c
Illbur <illbur@localhost.localdomain>
2017-12-19 11:04:45 -0500

    adding more

---
+++ linked_list/add_node.C
@@ -0,0 +1,13 @@
+# this is a function to add a node to a linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node* next;
+}
+int main(){
+  struct node* t->data = 5;
+  printf("%d", t->data);
+}
---
+++ printN.C
@@ -0,0 +1,9 @@
+/* this is a function to print a statment N number of times
+*/
+#include <stdio.h>
+
+int main(){
+  for(int i =0; i <10; i ++){
+    printf("\n This is the Line to be printed");
+  }
+}
